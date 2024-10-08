62dfd8573e220cc7e1e02417e792459a81065824
Braedon Fredin <b260fredin@gmail.com>
2024-10-08 09:26:29 -0500

    [2.05] Display categories

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class ProductController : Controller
+{
+  // this controller depends on the NorthwindRepository
+  private DataContext _dataContext;
+  public ProductController(DataContext db) => _dataContext = db;
+  public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
+}
