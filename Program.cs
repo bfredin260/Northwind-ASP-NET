5e1a29a2000a07d4616cde3f20c70b36a0a5a3ec
Braedon Fredin <b260fredin@gmail.com>
2024-10-08 09:01:49 -0500

    [1.02] Configure MVC project

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
