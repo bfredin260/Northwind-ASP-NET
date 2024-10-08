34cc9fca0b1335cbd19dc1f0a25019cba55dd328
Braedon Fredin <b260fredin@gmail.com>
2024-10-08 09:23:33 -0500

    [2.03] Register Data context

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+// Connection info stored in appsettings.json
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+// Register the DataContext service
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(configuration["Data:Northwind:ConnectionString"]));
 var app = builder.Build();
 
 app.UseHttpsRedirection();
