730a8a2808ecef257b88351706d828d5785b6f61
Braedon Fredin <b260fredin@gmail.com>
2024-10-08 09:50:24 -0500

    [4.10] Display discount code/title

--- wwwroot/discount.js
+++ wwwroot/discount.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('discount-row').addEventListener('click', function(e){
         if (e.target.classList.contains('discount')) {
             e.preventDefault();
+            document.getElementById('code').innerHTML = e.target.dataset['code'];
+            document.getElementById('title').innerHTML = e.target.dataset['title'];
             bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
         }
     });
