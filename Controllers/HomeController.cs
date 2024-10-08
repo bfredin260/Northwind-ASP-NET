7badb55481176538b69b130e6b71f730410ac9ba
Braedon Fredin <b260fredin@gmail.com>
2024-10-08 09:02:36 -0500

    [1.03] Home controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index() => "This is Home";
+    }
+}
