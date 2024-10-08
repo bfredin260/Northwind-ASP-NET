38bdf65dec6b8a4bda9318ffbea92495afeedec3
Braedon Fredin <b260fredin@gmail.com>
2024-10-08 09:19:06 -0500

    [2.01] Entity framework / Data model

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public string Description { get; set; }
+    public ICollection<Product> Products { get; set; }
+ }
+ 
