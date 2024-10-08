a80ec69cccb8ba164f988c55e71ea4a73cfd1fd1
Braedon Fredin <b260fredin@gmail.com>
2024-10-08 09:45:07 -0500

    [4.07] Create event listener

--- wwwroot/discount.js
+++ wwwroot/discount.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+    document.getElementById('discount-row').addEventListener('click', function(e){
+        e.preventDefault();
+        bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+    });
 });
