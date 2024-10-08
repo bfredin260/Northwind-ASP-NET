836360f5b7b84ea7e78fa61006cd885aabdba8ea
Braedon Fredin <b260fredin@gmail.com>
2024-10-08 09:31:20 -0500

    [4.01] Modify schema + migrations

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -3,7 +3,8 @@ using Microsoft.EntityFrameworkCore;
 public class DataContext : DbContext
 {
   public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-  
+
   public DbSet<Product> Products { get; set; }
   public DbSet<Category> Categories { get; set; }
+  public DbSet<Discount> Discounts { get; set; }
 }
