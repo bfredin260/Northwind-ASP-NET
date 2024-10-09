b0afe3da92d7d5fef875cf50259ca1270c0d67ef
Braedon Fredin <b260fredin@gmail.com>
2024-10-09 00:17:47 -0500

    [4.13] Added Current Discounts page to display active discounts

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,6 +5,7 @@ public class ProductController : Controller
   // this controller depends on the NorthwindRepository
   private DataContext _dataContext;
   public ProductController(DataContext db) => _dataContext = db;
+  public IActionResult Discount() => View(_dataContext.Discounts.OrderBy(d => d.DiscountId).Where(d => d.StartTime < DateTime.Now && d.EndTime > DateTime.Now));
   public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
   public IActionResult Index(int id) => View(_dataContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
 }
