537efc079978f965a857b729dd571986bcdeaacc
Braedon Fredin <b260fredin@gmail.com>
2024-10-21 21:13:00 -0500

    [5.01] Customer Model

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -7,4 +7,5 @@ public class DataContext : DbContext
   public DbSet<Product> Products { get; set; }
   public DbSet<Category> Categories { get; set; }
   public DbSet<Discount> Discounts { get; set; }
+  public DbSet<Customer> Customers { get; set; }
 }
