61814a5e2a35d0cf5917e35005a6907eba6e76d0
Braedon Fredin <b260fredin@gmail.com>
2024-10-21 21:13:13 -0500

    [5.01.01] Customer Model - real

---
+++ Models/Customer.cs
@@ -0,0 +1,13 @@
+public class Customer
+{
+    public int CustomerId { get; set; }
+    public string CompanyName { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string Region { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public string Phone { get; set; }
+    public string Fax { get; set; }
+    public string Email { get; set; }
+}
